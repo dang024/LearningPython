34be0b5c6c0129695ca20784c763fd33a13ee517
dang024 <dangosimpson@gmail.com>
2018-03-28 11:52:35 -0400

    Initial

---
+++ SlagathorsAdventure.py
@@ -0,0 +1,7 @@
+print """
+Welcome to Slagathor's Adventure!
+Death and Misfortune await.
+"""
+
+# Characters named Death and Misfortune greet the player at the beginning, maybe in a bar or some other loud location
+# Altercation occurs causing player to either intervene or be passive about it
---
+++ ex1.py
@@ -0,0 +1,9 @@
+#This is a comment
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print 'Donny said he was the "king"'
